be26e6fa458edb49297a9369c8da1c003e6c42a1
Robbo00 <rscrog606@west-mec.org>
2025-04-25 18:18:43 -0700

    r

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-for (let i = 0; i < 20; i++) {
+for (let i = 1; i <= 1302; i++) {
 let red = fetch('https://pokeapi.co/api/v2/pokemon?limit=250')
     
 .then(response =>{
@@ -9,8 +9,11 @@ let red = fetch('https://pokeapi.co/api/v2/pokemon?limit=250')
 })
 
 .then(data =>{
-let yes = []
-        console.log("Name: ", data.results[i].name)
+    let id = [i]
+let yes = []        
+console.log(i)
+
+        console.log("Name: ", data.results[i][0])
         yes.push(data.results[i].name)
         data
     // console.log(data.charizard)
@@ -20,12 +23,26 @@ let blue = fetch(data.results[i].url)
         return response.json()
     })
     .then(data=>{
-        console.log(data.base_experience)
+        let get = yes[0]
+        yes = []
+        yes.push(get, data.base_experience, data.sprites.front_default, data.abilities[0].ability.name, data.types[0].type.name)
+        console.log(yes)
+        make(yes, i)
         return data
     })
     return data
     
 })
-
 }
 
+function make(info, id){
+    let backo = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+    let template = `<div id="${info[0]}" class="P ${info[4]}"> <h1 id="Phead" class="red" >${info[0]}</h1> <div class="disp"><p>${info[3]}<p/> <p>${info[4]}<p/> </div> </div>`
+    let reg = document.createElement('div')
+    reg.innerHTML = template
+    document.getElementById("outp").appendChild(reg)
+    background(backo, info[0])
+}
+function background(back, name){
+    document.getElementById(name).style.backgroundImage = `url(${back})`
+}
