f34dd30b9c43880ae0b4ba74ec5fc70f552801c1
Robbo00 <rscrog606@west-mec.org>
2025-04-28 18:20:31 -0700

    red

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ let blue = fetch(data.results[i].url)
 
 function make(info, id){
     let backo = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
-    let template = `<div id="${info[0]}" class="P ${info[4]}"> <h1 id="Phead" class="red content" >${info[0].charAt(0).toUpperCase() + info[0].slice(1)}</h1> <div class="disp"> <p class="content h">${info[3].charAt(0).toUpperCase() + info[3].slice(1)} Type<p/> <p class="content">1st Ability: ${info[4].charAt(0).toUpperCase() + info[4].slice(1)}</p> <p class="content">Base Exp ${info[1]}</p></div> </div>`
+    let template = `<div ondblclick="Stay(id)" id="${info[0]}" class="P ${info[4]}"> <h1 id="Phead" class="red content" >${info[0].charAt(0).toUpperCase() + info[0].slice(1)}</h1> <div class="disp"> <p class="content h">${info[3].charAt(0).toUpperCase() + info[3].slice(1)} Type<p/> <p class="content">1st Ability: ${info[4].charAt(0).toUpperCase() + info[4].slice(1)}</p> <p class="content">Base Exp ${info[1]}</p></div> </div>`
     let reg = document.createElement('div')
     reg.innerHTML = template
     document.getElementById("outp").appendChild(reg)
@@ -47,4 +47,31 @@ function make(info, id){
 }
 function background(back, name){
     document.getElementById(name).style.backgroundImage = `url(${back})`
+}
+
+function Stay(event){
+    
+    let red = document.getElementById(event)
+    let blue = red.children
+    if(red.name != red.getAttribute('id')){
+        for (let i = 0; i < blue.length; i++) {
+        blue[i].style.opacity = 1
+        blue[i].style.transform = 'rotateY(-180deg)'
+
+    }
+    red.style.transform = "rotateY(180deg)"
+    
+    console.log(red.getAttribute('name'))
+    red.setAttribute('name', red.getAttribute('id'))
+    }
+    else{
+        red.style.transform = "rotateY(-180deg)"
+        red.setAttribute('name', '')
+        for (let i = 0; i < blue.length; i++) {
+            blue[i].style.opacity = 0
+            blue[i].style.transform = 'rotateY(180deg)'
+    
+        }
+    }
+    
 }
