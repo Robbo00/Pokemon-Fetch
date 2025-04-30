11cec2d99daf7227c8197707369dd431c8843617
Robbo00 <rscrog606@west-mec.org>
2025-04-29 21:33:52 -0700

    l

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Initializes Variables
 let aud = new Audio('coin-flip-shimmer-85750.mp3')
-let yes = prompt("How much pokemon")
+let yes = Number(prompt("How much pokemon"))
 let pokeDex = []
 let favy = []
+//Gets prompt and puts it in for loop to determine how many pokemon to get
 for (let i = 0; i < yes; i++) {
 let red = fetch('https://pokeapi.co/api/v2/pokemon?limit=1302')
 
@@ -15,13 +17,9 @@ let red = fetch('https://pokeapi.co/api/v2/pokemon?limit=1302')
 .then(data =>{
     let id = [i]
 let yes = []        
-console.log(i)
 
-        console.log("Name: ", data.results[i][0])
         yes.push(data.results[i].name)
         data
-    // console.log(data.charizard)
-    // console.log("Ability: ", data.abilities)
 let blue = fetch(data.results[i].url)
     .then(response=>{
         return response.json()
@@ -30,28 +28,34 @@ let blue = fetch(data.results[i].url)
         let get = yes[0]
         yes = []
         yes.push(get, data.base_experience, data.sprites.front_default, data.abilities[0].ability.name, data.types[0].type.name)
-        console.log(yes)
-        make(yes, i + 1)
+        make(yes, i + 1, data.height, data.weight)
         return data
     })
     return data
     
 })
 }
-
-function make(info, id){
+// Function that gets called after every single iteration of the for loop above to crete the pokemon element in th output
+function make(info, id, height, weight){
     pokeDex.push([info[0], info[2]])
     let backo = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
-    let template = `<div ondblclick="fav(this.id)" ondblclick="Stay(id)" id="${info[0]}" class="P ${info[4]}"> <h1 id="Phead" class="red content" >${info[0].charAt(0).toUpperCase() + info[0].slice(1)}</h1> <div class="disp"> <p class="content h">${info[3].charAt(0).toUpperCase() + info[3].slice(1)} Type<p/> <p class="content">1st Ability: ${info[4].charAt(0).toUpperCase() + info[4].slice(1)}</p> <p class="content">Base Exp ${info[1]}</p></div> </div>`
+    let template = `<div ondblclick="fav(this.id)" ondblclick="Stay(id)" id="${info[0]}" class="P ${info[4]}"> <h1 id="Phead" class="red content" >${info[0].charAt(0).toUpperCase() + info[0].slice(1)}</h1> <div class="disp"> <p class="content h">${info[3].charAt(0).toUpperCase() + info[3].slice(1)} Type<p/> <p class="content">1st Ability: ${info[4].charAt(0).toUpperCase() + info[4].slice(1)}</p> <p class="content">Base Exp ${info[1]}</p> <p class="content">ID: ${id}</p> <p class="content">Height: ${height}</p> <p class="content">Weight ${weight}</p></div> </div>`
     let reg = document.createElement('div')
     reg.innerHTML = template
     document.getElementById("outp").appendChild(reg)
     background(backo, info[0])
-
+    
 }
-function background(back, name){
+// Sets background fro previously created cards to the image
+function background(back, name){    
+    let lasto = document.getElementById('outp').children
+
     document.getElementById(name).style.backgroundImage = `url(${back})`
-    console.log(pokeDex)
+    if(document.getElementById('outp').childElementCount >= yes){
+        for (let i = 0; i <= lasto.length + 1; i++) {
+           lasto[i].classList.add('show')
+        }
+    }
 }
 
 // function red(){
@@ -68,13 +72,9 @@ function background(back, name){
 //     }
 //     red.style.transform = "rotateY(180deg)"
     
-//     console.log(red.getAttribute('name'))
 //     red.setAttribute('name', red.getAttribute('id'))
-//     console.log(red.getAttribute('name'))
-//     console.log(red.getAttribute('id'))
 //     }
 //     else{
-//         console.log('red blue')
 //         red.style.transform = "rotateY(-180deg)"
 //         red.setAttribute('name', '')
 //         for (let i = 0; i < blue.length; i++) {
@@ -85,7 +85,7 @@ function background(back, name){
 //     }
     
 // }
-
+// Gets the matching pokemon up to 6 search results by cutting the pokemon name to the search values length and comparing
 function ent(event, id){  
     let cont = document.getElementById('auto')
     while (cont.childElementCount > 2) {
@@ -96,9 +96,8 @@ function ent(event, id){
 
     }
     catch{
-        console.log('empty')
     }
-    let search = document.getElementById(id).value
+    let search = document.getElementById(id).value.toLowerCase()
     if(search == ''){
         return
     }
@@ -109,56 +108,55 @@ function ent(event, id){
     let el = ''
     for (let i = 0; i < pokeDex.length; i++ || res == 6) {
         el = document.createElement('p')
-         temp = `<p name="SR" id="${pokeDex[i][0]}SR">${pokeDex[i][0]} <img onclick="load(this)" class="${pokeDex[i][0]}" src="${pokeDex[i][1]}"></p>`
+         temp = `<p class="${pokeDex[i][0]}" id="${pokeDex[i][0]}SR">${pokeDex[i][0]} <img onclick="load(this)" class="${pokeDex[i][0]}" src="${pokeDex[i][1]}"></p>`
          el.innerHTML = temp
         comp = pokeDex[i][0]
         breako = search.length
         comp = comp.split('')
         comp = comp.slice(0, breako)
-        console.log(comp.join(''))
-        console.log(search)
         comp = comp.join('')
-        console.log(comp)
-        console.log(search)
         if(res.length == 6){
             return
         }
         if(search == comp){
             res.push(pokeDex[i])
             document.getElementById('auto').appendChild(el)
-            document.getElementById(`${pokeDex[i][0]}SR`).setAttribute('class', 'animato')
+            document.getElementById(`${pokeDex[i][0]}SR`).setAttribute('class', `${pokeDex[i][0]}`)
         }
         
     }
-    console.log(res)
-    if(event.key === 'Enter'){
-      
-        console.log(search)
-    }
+    
 }
+// Checks if the enter button was pressed or inf] a sub image was clicked on 
 function load(id){
-    console.log('robert')
-    let searched = document.getElementById(id.className)
-    console.log(searched)
+    let searched = ''
+    if(id == 'Enter'){
+             searched = document.getElementById('auto').children[2]
+             searched = searched.firstChild.getAttribute('class')
+             searched = document.getElementById(searched)
+    }
+    else{
+         searched = document.getElementById(id.className)
+    }
     let it = document.getElementById('outp')
     for (let i = 0; i < it.children.length; i++) {
             it.children[i].style.display = 'none'
-            console.log(it.children[i])
             
     }
-    console.log(searched)
     searched.parentElement.style.display = 'initial'
 //     for (let i = 0; i < it.children.length; i++) {
 //         it.children[i].style.display = 'initial'
 
 // }
 }
+// brings back the pokeDex
 function back(){
     let ito = document.getElementById('outp')
     for (let i = 0; i < ito.children.length; i++) {
             ito.children[i].style.display = 'initial'
     }
 }
+// on dbl click lights up the card gold and puts a gold glow
 function fav(ev){
     let dbl = document.getElementById(ev)
     if(dbl.style.backgroundColor != 'gold'){
@@ -173,9 +171,10 @@ function fav(ev){
         favy.splice(favy.indexOf(dbl), 1)
     }
     
-
+    localStorage.setItem('fav', favy)
     
 }
+// displays favorites hides everything else
 function pFav(){
     if(favy.length == 0){
         return
@@ -183,16 +182,32 @@ function pFav(){
     let bo = document.getElementById('outp')
     for (let i = 0; i < bo.children.length; i++) {
         bo.children[i].style.display = 'none'
-        console.log(bo.children[i])
 }
     for (let i = 0; i < favy.length; i++) {
         favy[i].parentElement.style.display = 'initial'
-        console.log(bo.children[i])
 }
 }
+//gets rid of search results on blur
 function blurry(){
+    setTimeout(() => {
+    }, 1000);
     let cont = document.getElementById('auto')
     while (cont.childElementCount > 2) {
         cont.removeChild(cont.lastChild)
     }
 }
+// clears the background color of the favorites and sets 
+function cFav(){
+    for (let i = 0; i < favy.length; i++) {
+        favy[i].style.backgroundColor = 'salmon'
+        favy[i].style.boxShadow = ''        
+    }
+    favy = []
+}
+// Sees every button press and checks if its enter if not jus go back and if it is call in the 
+function checkEnt(event){
+    if(event.key === 'Enter'){
+        load(event.key)
+    }
+}
+
